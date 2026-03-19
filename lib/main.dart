72b2b7ed387638dac2a6c404080a225a0cd8fbde
Silvestre Alexander Olvera Rocha <22690470@tecvalles.mx>
2026-03-19 12:46:06 -0600

    Splash Screen a medias
    Corregir a como esta en figma

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,16 @@ import 'package:flutter/material.dart';
 import 'app.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const Main());
+}
+
+class Main extends StatefulWidget{
+  const Main (superkey)
+}
+
+class _MainState extends State<main>{
+  @override
+  Widget build(BuildContext context){
+    
+  }
 }
