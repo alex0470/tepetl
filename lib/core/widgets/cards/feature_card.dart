e5ee852f4ff20d14d840be5e1790e9e83c971d25
Silvestre Alexander Olvera Rocha <22690470@tecvalles.mx>
2026-03-21 01:02:45 -0600

    as

--- lib/core/widgets/cards/feature_card.dart
+++ lib/core/widgets/cards/feature_card.dart
@@ -24,7 +24,7 @@ class FeatureCardDark extends StatelessWidget {
         borderRadius: BorderRadius.circular(16),
         boxShadow: [
           BoxShadow(
-            color: Colors.black.withOpacity(0.3),
+            color: Colors.black.withValues(alpha: 0.3),
             blurRadius: 2,
             offset: const Offset(2, 2),
           )
